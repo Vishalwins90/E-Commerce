c50bf8f6766aafbbe089dabc823169b2720d5549
Vishal <Vishalwins90@gmail.com>
2023-08-07 16:21:48 +0530

    Added my project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { CartComponent } from './components/cart/cart.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { InterceptorsComponent } from './components/interceptors/interceptors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductListComponent,
+    CartComponent,
+    UserProfileComponent,
+    InterceptorsComponent
   ],
   imports: [
     BrowserModule,
